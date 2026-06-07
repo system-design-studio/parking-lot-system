a1f729bc17124349559da921c87951d555a42739
chakravarthyBatna <chakravarthybatna46234@gmail.com>
2026-06-07 20:55:43 +0530

    first commit

---
+++ src/test/java/com/sds/parkinglotsystem/ParkingLotSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.parkinglotsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
